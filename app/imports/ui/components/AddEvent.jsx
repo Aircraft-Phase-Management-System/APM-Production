c953146f33aa5dd3fdd7cc3ec5ffa880b3d122b0
acatarinaoaraujo <acoa_@hotmail.com>
2023-03-10 14:12:04 -1000

    inital form (add event)

--- app/imports/ui/components/AddEvent.jsx
+++ app/imports/ui/components/AddEvent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import { Col, Container, Row, Button, Form, InputGroup } from "react-bootstrap";
 import {
   AutoForm,
   ErrorsField,
@@ -37,6 +37,29 @@ const AddEvent = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    console.log(event);
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  const onFormChange = (e) => {
+    const value = e.target.value;
+    console.log(value);
+  };
+
+  const onFormClick = (e) => {
+    const value = e.target.value;
+    console.log(value);
+  };
+
   // On submit, insert the data.
   const submit = (data, formRef) => {
     const { title, end, start, bgColor } = data;
@@ -61,44 +84,97 @@ const AddEvent = () => {
       </Button>
 
       <Modal show={show} onHide={handleClose}>
-        <AutoForm
+        {/*<AutoForm
           ref={(ref) => {
             fRef = ref;
           }}
           schema={bridge}
           onSubmit={(data) => submit(data, fRef)}
-        >
-          <Modal.Header closeButton>
+        >*/}
+        <Modal.Header closeButton>
           <Modal.Title as="h5">Add New Event</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Container id={PAGE_IDS.ADD_EVENT} className="py-3">
-              <TextField name="title" />
-              <Row>
-                <Col>
-                  {" "}
-                  <TextField name="start" />
-                </Col>
-
-                <Col>
-                  <TextField name="end" />
-                </Col>
+        </Modal.Header>
+        <Modal.Body>
+          <Container id={PAGE_IDS.ADD_EVENT} className="py-3">
+            <Form ref={(ref) => {
+            fRef = ref;
+          }}
+          schema={bridge}
+          onSubmit={(data) => submit(data, fRef)} noValidate validated={validated} /*onSubmit={handleSubmit}*/>
+              <Row className="mb-3">
+                <Form.Group controlId="validationCustom01">
+                  <Form.Label>Event Title</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    name="title"
+                    placeholder="Ex: PMI 1"
+                    onChange={onFormChange}
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
               </Row>
-              <Col></Col>
-              <SelectField name="bgColor" />
-              <SubmitField value="Submit" />
-              <ErrorsField />
-            </Container>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </AutoForm>
+
+              <Row className="mb-3">
+                <Form.Group as={Col} md="6" controlId="validationCustom02">
+                  <Form.Label>Start Date</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="start"
+                    placeholder="Ex: 2023-03-15"
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid date.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                {/*
+                <Form.Group as={Col} md="6" controlId="validationCustom03">
+                  <Form.Label>Required Number of Days</Form.Label>
+                  <Form.Control type="text" name="daysNum" placeholder="Ex: 20" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid number.
+                  </Form.Control.Feedback>
+                </Form.Group>
+      */}
+              </Row>
+
+              <Row className="mb-3">
+                <Button>Calculate Date</Button>
+              </Row>
+
+              <Row className="mb-3">
+                <Form.Group>
+                  <Form.Label>End Date</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="end"
+                    placeholder="Ex: 2023-03-15"
+                  />
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group>
+                  <Form.Label>Event Color</Form.Label>
+                  <Form.Control type="text" name="color" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid color.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Button type="submit">Submit form</Button>
+            </Form>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+        {/* </AutoForm> */}
       </Modal>
     </>
   );
