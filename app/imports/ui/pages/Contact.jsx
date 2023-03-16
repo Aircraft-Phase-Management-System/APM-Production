6b4b1da14a6ad0e9b9b6d4f262d9f7b3b540a184
jingzhef <jingzhef@hawaii.edu>
2023-03-15 14:54:36 -1000

    Add support page and modified the landing page and get start button

--- app/imports/ui/pages/Contact.jsx
+++ app/imports/ui/pages/Contact.jsx
@@ -3,7 +3,7 @@ import { Button, Container, Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 /** Render a Contact page page if the user wants to contact any of the Developers in from the team or reach out to us. */
-class ContactUs extends React.Component {
+class Contact extends React.Component {
   render() {
     const contactStyle = { marginLeft: '20px', marginRight: '20px' };
     return (
@@ -15,23 +15,31 @@ class ContactUs extends React.Component {
             <hr/>
           </Header>
           <div style={contactStyle}>
-            <p>Thank you for visiting the University of Hawaii at Manoa Students Club Association website.
-              We exist to provide a voice and community for UH students. All current students can register a account in
+            <p>Thank you for visiting the Aircraft Phase Management System.
+              We exist to provide convienvent on managing phase lanes for the Aircraft community. All new users can register an account at
             <Link to="/signup"> here </Link>
               .</p>
             <p>
-              Please be aware we cannot provide assistance for academic matters. For these queries, please visit the
-              <a target="_blank" href="https://manoa.hawaii.edu/about/contact/" rel="noreferrer"> UHM Contact Page</a>
+              Please be aware we cannot provide assistance for duties outside the Scheduler. 
+              For these queries, please contact your supervisor directly or our sponsor Lt Dante Carrillo 
+              (<a target='_blank' href=''>dante.carrillo01@gmail.com</a>) 
+              for further assistance
             </p>
 
-            <Header>If you would like to contact us, please email: jingzhef@hawaii.edu
+            <Header>If you encounter to any bugs or issues and would like to contact us, please email: 
+                (<a target='_blank' href=''>jingzhef@hawaii.edu </a>)  
+                or 
+                (<a target='_blank' href=''>acoa@hawaii.edu </a>) 
             </Header>
 
             <Header>For Instructions and more details about the site, Please visit
-              <a target="_blank" href="https://the-club-for-you.github.io/" rel="noreferrer"> Our Home page</a>
+              <a target="_blank" href="http://ics-software-engineering.github.io/meteor-application-template-production/" rel="noreferrer"> Meteor-Application-Template-React</a>
             </Header>
-            <p>The website is still under development, please provide any issues encounted or suggestions. Thanks for building the club community better :)</p>
-            <Button color='green' href="https://forms.gle/QNDnx35H4zPpbf486">Community Feedback</Button>
+            <div>
+                <p>The website is still under development, feel free to provide any suggestions or report any issues encountered.</p>
+                <p>Thanks for building the Aircraft community better :)</p>
+            </div>
+
           </div>
           <br/>
         </Container>
